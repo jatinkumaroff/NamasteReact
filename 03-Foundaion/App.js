d427cd7ba4622d65102c42afcf3af5699120a355
Jatin Kumar <jatinkumaroff@gmail.com>
2026-02-03 16:12:13 +0530

    03 foundation done

--- 03-Foundaion/App.js
+++ 03-Foundaion/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//react element
-
 const heading = React.createElement("h1",{id:"heading"},"this is react heading");
 
 const root =ReactDOM.createRoot(document.getElementById("root"))
 
+// using one compponent in multiple areas:: THIS IS COMPONENT COMPOSITION
+const Component1= ()=><h1>this is component number 1</h1>
+const Component2= ()=><h1>this is called using tag</h1>
+const Component3= ()=><h1>this is called using function</h1>
+const HeadingComponent=() =>(
+    <div>
+     {98+89}
+     <Component1/>              {/* calling simply using tags*/}
+     <Component2/>              {/* calling simply using tags*/}
+     {Component3()}             {/*calling using a function*/}
+     </div>
+)   
+
 
 //js/broswer/reatc can NOT understnad jsx , its (babel in)parcel that transpiles it for js to understnad
 //that is jsx ===transpiled===> React.createElement - rest same  
-const jsxHeading = <h1 id="jsxHeading"> this is jsx </h1>;
+// const jsxHeading = <h1 id="jsxHeading"> this is jsx </h1>;
 
-// root.render(jsxHeading)
-root.render(heading)
+
+root.render(<HeadingComponent />)
+// root.render(<jsxHeading/>)
+
+
